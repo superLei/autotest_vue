f31b1b8f00798846532b0af4290d75feb3a6743c
苏磊 <sulei@hualala.com>
2018-03-19 19:35:50 +0800

    update3

--- src/components/page/charts2.vue
+++ src/components/page/charts2.vue
@@ -1,6 +1,6 @@
 <template>
      <div>
-    <ve-ring :data="chartData" :settings="chartSettings"></ve-ring>
+    <ve-ring :data="chartData" :settings="chartSettings" :colors="chartColors" legend-position="bottom" tooltip-visible="false"></ve-ring>
   </div>
 </template>
 <!--https://elemefe.github.io/v-charts/#/pie-->
@@ -9,25 +9,29 @@ import VeRing from 'v-charts/lib/ring'
 export default {
   created: function () {
     this.chartData = {
-      columns: ['日期', '成本', '利润'],
+      columns: [ '成功','失败'],
       rows: [
-        { '日期': '1月1号', '成本': 123, '利润': 3 },
-        { '日期': '1月2号', '成本': 1223, '利润': 6 },
-        { '日期': '1月3号', '成本': 2123, '利润': 9 },
-        { '日期': '1月4号', '成本': 4123, '利润': 12 },
-        { '日期': '1月5号', '成本': 3123, '利润': 15 },
-        { '日期': '1月6号', '成本': 7123, '利润': 20 },
-        { '日期': '1月7号', '成本': 4123, '利润': 20 },
-        { '日期': '1月8号', '成本': 1123, '利润': 20 },
-        { '日期': '1月9号', '成本': 5223, '利润': 20 },
-        { '日期': '1月1号0', '成本': 9123, '利润': 20 },
-        { '日期': '1月1号1', '成本': 4123, '利润': 20 }
+        {'成功': 99, '失败': 1 },
+        {'成功': 1, '失败': 99 },
+ 
       ]
     }
     this.chartSettings = {
-      limitShowNum: 1
+      limitShowNum: 2,
+      hoverAnimation:false,
+      labelLine:{
+        show:false,
+        length:1
+      },
+      label:{
+        formatter: '{b}{d}',
+        color:'#303133'
+      }
     }
+    this.chartColors = ['#67C23A', '#F56C6C']
   },
+ 
+  
   components: { VeRing }
 
 }
---
+++ src/components/page/charts3.vue
@@ -0,0 +1,32 @@
+<template>
+     <div>
+    <ve-ring :data="chartData" :settings="chartSettings" :colors="chartColors" legend-position="bottom" tooltip-visible="false"></ve-ring>
+  </div>
+</template>
+<!--https://elemefe.github.io/v-charts/#/pie-->
+<script>
+import VeRing from 'v-charts/lib/ring'
+export default {
+  created: function () {
+    this.chartData = {
+      columns: [ '成功','失败'],
+      rows: [
+        {'成功': 99, '失败': 1 },
+        {'成功': 1, '失败': 99 },
+ 
+      ]
+    }
+    this.chartSettings = {
+      limitShowNum: 1
+    }
+    this.chartColors = ['#67C23A', '#F56C6C',]
+  },
+  
+  components: { VeRing }
+
+}
+</script>
+
+<style scoped>
+
+</style>
